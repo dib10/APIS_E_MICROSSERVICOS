ca058387f1309665e7d587e9812ebf15a03e29c4
Caio <caiodib10@gmail.com>
2025-03-07 17:08:14 -0300

    upload

--- lista02/l2ex14/src/l2ex14/Main.java
+++ lista02/l2ex14/src/l2ex14/Main.java
@@ -1,9 +1,31 @@
 package l2ex14;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Digite a primeira nota: ");
+		double nota1 = sc.nextDouble();
+		
+		System.out.println("Digite a segunda nota");
+		double nota2 = sc.nextDouble();
+		
+		System.out.println("Digite a terceira nota: ");
+		double nota3 = sc.nextDouble();
+		
+		double media = MediaFinal.calcularMedia(nota1, nota2, nota3);
+		
+		//imprimir resultado
+		
+		String resultado = MediaFinal.Status(media);
+		
+		System.out.println("Média" + media + "," + resultado);
+		
 
 	}
 
--- lista02/l2ex14/src/l2ex14/MediaFinal.java
+++ lista02/l2ex14/src/l2ex14/MediaFinal.java
@@ -1,5 +1,22 @@
 package l2ex14;
 
 public class MediaFinal {
+	
+	public static double calcularMedia(double nota1, double nota2, double nota3) 
+	{
+		return(nota1+nota2+nota3)/3;
+	}
+	
+	public static String Status(double media) {
+		if(media > 7) {
+			return "aprovado";
+		}
+		else if (media <3) {
+			return "reprovado";
+		}
+		else {
+			return "Exame";
+		}
+	}
 
 }
