fe66bd7d53f0508e6ccae675674323a6e14751a3
Caio <caiodib10@gmail.com>
2025-04-13 18:54:03 -0300

    Início projeto

---
+++ tasks_api/src/main/java/dev/caio/tasks_api/TasksApiApplication.java
@@ -0,0 +1,13 @@
+package dev.caio.tasks_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TasksApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TasksApiApplication.class, args);
+	}
+
+}
