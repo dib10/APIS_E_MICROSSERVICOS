d328e75c690e5e1b99c9d7a56e272125126ce27f
Caio <caiodib10@gmail.com>
2025-04-13 19:15:02 -0300

    Atributos e anotações inciais
    Definindo atributos e anotações inicias do arquivo Task.java

--- tasks_api/src/main/java/dev/caio/tasks_api/model/Task.java
+++ tasks_api/src/main/java/dev/caio/tasks_api/model/Task.java
@@ -1,5 +1,58 @@
 package dev.caio.tasks_api.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Task {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String titulo;
+	private String descricao;
+	
+	
+	//construtor padrão
+	public Task() {
+	}
+
+
+	public Long getId() {
+		return id;
+	}
+
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	
+	
+	
+	
+	
+	
 
 }
