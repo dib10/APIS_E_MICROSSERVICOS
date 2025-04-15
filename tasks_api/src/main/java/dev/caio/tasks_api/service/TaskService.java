4e349341f312a5b9de6bb95cc6895ab13e63016e
Caio <caiodib10@gmail.com>
2025-04-15 16:34:19 -0300

    configurando o TaskService
    Configurando o TaskService.java para usar o ModelMapper

--- tasks_api/src/main/java/dev/caio/tasks_api/service/TaskService.java
+++ tasks_api/src/main/java/dev/caio/tasks_api/service/TaskService.java
@@ -1,8 +1,13 @@
 package dev.caio.tasks_api.service;
 
+import java.time.LocalDate;
+
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import dev.caio.tasks_api.dto.CreateTaskDTO;
+import dev.caio.tasks_api.model.Task;
 import dev.caio.tasks_api.repository.TaskRepository;
 
 @Service
@@ -10,6 +15,24 @@ public class TaskService {
 	
 	@Autowired //injeta instância do TaskRepository
 	private TaskRepository taskRepository;
+	@Autowired // para o spring injetar o Bean ModelMapper
+	private ModelMapper modelMapper;
+	
+	public Task createTask(CreateTaskDTO createTaskDTO) {
+		if (createTaskDTO.getDataLimite() != null && createTaskDTO.getDataLimite().isBefore(LocalDate.now())) {
+			System.out.println("ERRO!! A data limite não pode ser anterior à data atual");
+			return null;
+			
+		}
+		
+		Task newTask = modelMapper.map(createTaskDTO, Task.class);
+		System.out.println("Salvando a tarefa " + newTask.getTitulo());
+		
+        Task savedTask = taskRepository.save(newTask);
+        System.out.println("Tarefa salva com ID: " + savedTask.getId());
+        return savedTask;
+
+	}
 	
 
 }
