0edcbd9db2451bc47514f6c2a321381d497606dd
Caio <caiodib10@gmail.com>
2025-04-15 15:34:34 -0300

    Adição da classe CreateTaskDTO
    Defini os atributos, getters e setters.
    
    Não usei Lombok

---
+++ tasks_api/src/main/java/dev/caio/tasks_api/dto/CreateTaskDTO.java
@@ -0,0 +1,49 @@
+package dev.caio.tasks_api.dto;
+
+import java.time.LocalDate;
+
+import dev.caio.tasks_api.enums.Prioridade;
+
+public class CreateTaskDTO {
+	
+	private String titulo;
+	private String descricao;
+	private Prioridade prioridade;
+	private LocalDate dataLimite;
+	private String categoria;
+	
+	
+	public String getTitulo() {
+		return titulo;
+	}
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+	public String getDescricao() {
+		return descricao;
+	}
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	public Prioridade getPrioridade() {
+		return prioridade;
+	}
+	public void setPrioridade(Prioridade prioridade) {
+		this.prioridade = prioridade;
+	}
+	public LocalDate getDataLimite() {
+		return dataLimite;
+	}
+	public void setDataLimite(LocalDate dataLimite) {
+		this.dataLimite = dataLimite;
+	}
+	public String getCategoria() {
+		return categoria;
+	}
+	public void setCategoria(String categoria) {
+		this.categoria = categoria;
+	}
+	
+	
+
+}
--- tasks_api/src/main/java/dev/caio/tasks_api/service/TaskService.java
+++ tasks_api/src/main/java/dev/caio/tasks_api/service/TaskService.java
@@ -1,8 +1,15 @@
 package dev.caio.tasks_api.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import dev.caio.tasks_api.repository.TaskRepository;
 
 @Service
 public class TaskService {
+	
+	@Autowired //injeta instância do TaskRepository
+	private TaskRepository taskRepository;
+	
 
 }
