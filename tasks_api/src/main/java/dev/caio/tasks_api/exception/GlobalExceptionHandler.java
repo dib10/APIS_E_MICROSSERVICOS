a0d9f1cb139687d7de54098cae52e52fc56d6e82
Caio <caiodib10@gmail.com>
2025-04-16 21:45:10 -0300

    ✨ Criando a exceção InvalidTaskStateException

--- tasks_api/src/main/java/dev/caio/tasks_api/exception/GlobalExceptionHandler.java
+++ tasks_api/src/main/java/dev/caio/tasks_api/exception/GlobalExceptionHandler.java
@@ -5,6 +5,8 @@ import java.util.Map;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -18,6 +20,28 @@ public class GlobalExceptionHandler {
 		body.put("message", ex.getMessage());
         System.out.println("ResourceNotFoundException: " + ex.getMessage());
         return new ResponseEntity<>(body, HttpStatus.NOT_FOUND);
+	}
+	
+	// (400) - Erros de Validação @Valid
+	
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public ResponseEntity<Map<String, String>> handleValidationExceptions(MethodArgumentNotValidException ex) {
+		
+		Map<String, String> errors = new HashMap<>();
+		//Iterando sobre todos os erros de validação encontrados
+		ex.getBindingResult().getAllErrors().forEach((error) -> {
+			String fieldName = ((FieldError)error).getField();
+			
+			String errorMessage = error.getDefaultMessage();
+			
+			errors.put(fieldName, errorMessage);
+		});
+		
+		//debug
+		System.out.println("Erro de validação capturado");
+			
+			return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
+		
 	}
 
 }
---
+++ tasks_api/src/main/java/dev/caio/tasks_api/exception/InvalidTaskStateException.java
@@ -0,0 +1,13 @@
+package dev.caio.tasks_api.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.CONFLICT)
+public class InvalidTaskStateException extends RuntimeException {
+	
+	public InvalidTaskStateException(String message) {
+		super(message);
+	}
+
+}
