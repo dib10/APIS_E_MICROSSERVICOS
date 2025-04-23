01a5824fc56d381009621c1e6d3905688c1a5669
Caio <caiodib10@gmail.com>
2025-04-23 13:36:42 -0300

    💡 Comentários

--- tasks_api/src/test/resources/cleanup.sql
+++ tasks_api/src/test/resources/cleanup.sql
@@ -1,3 +1,8 @@
 -- src/test/resources/cleanup.sql
+
+-- basicamente o script serve para limpar a tabela task do banco em memória, antes da execução dos testes para evitar erros
+
+-- apaga todos registros
 DELETE FROM task;
+-- reinicia a seq de numeração
 ALTER TABLE task ALTER COLUMN id RESTART WITH 1;
