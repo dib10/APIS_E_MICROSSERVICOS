0f6d21c51ba649a22c731c5cc61fc2af5aa21619
Caio <caiodib10@gmail.com>
2025-05-05 22:13:30 -0300

    🐛 Correção no script sql

--- tasks_api/src/main/java/dev/caio/tasks_api/service/UserService.java
+++ tasks_api/src/main/java/dev/caio/tasks_api/service/UserService.java
@@ -5,6 +5,7 @@ import java.util.Set;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
 
 import dev.caio.tasks_api.dto.dto.authentication.UserRegistrationDTO;
 import dev.caio.tasks_api.enums.ERole;
@@ -16,6 +17,7 @@ import dev.caio.tasks_api.repository.RoleRepository;
 import dev.caio.tasks_api.repository.UserRepository;
 import jakarta.transaction.Transactional;
 
+@Service
 public class UserService {
 	
 	@Autowired
