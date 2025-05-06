9e7b047e35268335c3ba5e65b21cacb201ef41f8
Caio <caiodib10@gmail.com>
2025-05-06 13:17:23 -0300

    🔧 config: Adiciona beans AuthenticationManager e JwtEncoder ao SecurityConfig

--- tasks_api/src/main/java/dev/caio/tasks_api/config/SecurityConfig.java
+++ tasks_api/src/main/java/dev/caio/tasks_api/config/SecurityConfig.java
@@ -1,17 +1,26 @@
 package dev.caio.tasks_api.config;
+import java.security.interfaces.RSAPrivateKey;
+import java.security.interfaces.RSAPublicKey;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-
-import org.springframework.security.web.SecurityFilterChain;
-import java.security.interfaces.RSAPublicKey;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.JwtEncoder;
 import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+import com.nimbusds.jose.jwk.JWKSet;
+import com.nimbusds.jose.jwk.RSAKey;
+import com.nimbusds.jose.jwk.source.ImmutableJWKSet;
 
 @Configuration 
 @EnableWebSecurity 
@@ -19,6 +28,9 @@ public class SecurityConfig {
 	
 	@Value("${jwt.public.key}")
 	private RSAPublicKey publicKey;
+	
+    @Value("${jwt.private.key}")
+    private RSAPrivateKey privateKey;
 
     @Bean
     public PasswordEncoder passwordEncoder() {
@@ -30,6 +42,22 @@ public class SecurityConfig {
     public JwtDecoder jwtDecoder() {
     	return NimbusJwtDecoder.withPublicKey(this.publicKey).build();
     }
+    
+    @Bean
+    public JwtEncoder jwtEncoder() {
+    	RSAKey jwk = new RSAKey.Builder(this.publicKey).privateKey(this.privateKey).build();
+  ImmutableJWKSet<com.nimbusds.jose.proc.SecurityContext> jwks = new ImmutableJWKSet<>(new JWKSet(jwk));
+  return new NimbusJwtEncoder(jwks);
+  
+    }
+    
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+    	
+    	return authenticationConfiguration.getAuthenticationManager();
+
+    }
+    
     
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
